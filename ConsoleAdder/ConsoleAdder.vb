3c701333c69fc91c7c24870442087727b8321e77
Alexey Nikitinsky <alexnn5@gmail.com>
2015-01-01 15:09:20 +0200

    Add ConsoleAdder application

---
+++ ConsoleAdder/ConsoleAdder.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+
+Module ConsoleAdder
+
+    Sub Main()
+        Const a As Integer = 1509
+        Const b As Integer = 744
+        Const c As Integer = a + b
+
+        Console.Write("The sum of ")
+        Console.Write(A)
+        Console.Write(" and ")
+        Console.Write(B)
+        Console.Write(" equals ")
+        Console.WriteLine(C)
+
+        Console.WriteLine("The SUB of " & A & " and " & B & " equals " & C)
+        Console.WriteLine("The sum of {0} and {1} equals {2}", A, B, C)
+
+    End Sub
+
+End Module
---
+++ ConsoleAdder/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleAdder")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ConsoleAdder")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7ab152b2-e9d3-4db3-93a4-f5e88e95b267")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
